f1a46aec2339d95c598f7fbb65bee8fee47a77b8
Jacob Lee <jkln3w@umsystem.edu>
2024-04-14 18:34:56 -0500

    Update 3 files
    - /prob_1_19a.py
    - /prob_1_18f.py
    - /prob_1_18h.py

--- prob_1_18f.py
+++ prob_1_18f.py
@@ -1,4 +1,4 @@
 from automata.fa.nfa import NFA
 
-prob_1_18f = "(0|(01))*(1|ϵ)"
+prob_1_18f = "(0*1*01*1*)*0*"
 
--- prob_1_18h.py
+++ prob_1_18h.py
@@ -1,4 +1,4 @@
 from automata.fa.nfa import NFA
 
-prob_1_18h = ""
+prob_1_18h = "0* (1 (01+ | 0 | ε) | ε)"
 
