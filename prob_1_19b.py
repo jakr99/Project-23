90ebc0b84a5e057add02eeda0d88d482d0fc9dd5
Jacob Lee <jkln3w@umsystem.edu>
2024-04-14 18:38:45 -0500

    Update file prob_1_19b.py

--- prob_1_19b.py
+++ prob_1_19b.py
@@ -1,17 +1,16 @@
 from automata.fa.nfa import NFA
 
 prob_1_19b = NFA(
-    states={'q0', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6'},
+    states={'q0', 'q1', 'q2', 'q3', 'q4', 'q5'},
     input_symbols={'0', '1'},
     transitions={
-        'q0': {'0': {'q1', 'q3'}, '1': {'q0'}, '': {'q6'}},  # Transition to q1 on 0, q3 on 0, or stay on q0 on epsilon
-        'q1': {'0': {'q2'}},                                 # Transition to q2 on 0 (part of '00')
-        'q2': {'1': {'q6'}},                                 # Transition to q6 on 1 (if '00' is followed by '11')
-        'q3': {'1': {'q4'}},                                 # Transition to q4 on 1 (part of '01')
-        'q4': {'': {'q6'}},                                  # Epsilon transition to q6
-        'q5': {'1': {'q6'}},                                 # Transition to q6 on 1 (completing '11')
-        'q6': {'': {'q0'}},                                  # Epsilon transition back to q0 to repeat the sequence
+        'q0': {'0': {'q1', 'q5'}, '': {'q0'}},
+        'q1': {'0': {'q2', 'q3'}},
+        'q2': {'1': {'q0'}},  # This accounts for the (00)* part
+        'q3': {'1': {'q4'}},  # This is the start of the (11) part
+        'q4': {'': {'q0'}},   # This transition completes the (00)*(11) and loops back for repetition
+        'q5': {'1': {'q0'}}   # This accounts for the 01 part
     },
     initial_state='q0',
-    final_states={'q0', 'q6'}  # q0 is accepting because the sequence can be empty; q6 because it completes any valid sequence
+    final_states={'q0'}
 )
