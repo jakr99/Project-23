2be669b10f2a13a599cbecc49f3d68fd659fe7c9
Jacob Lee <jkln3w@umsystem.edu>
2024-04-14 22:51:51 -0500

    Update file prob_1_18h.py

--- prob_1_18h.py
+++ prob_1_18h.py
@@ -1,17 +1,4 @@
-from automata.fa.dfa import DFA
+from automata.fa.nfa import NFA
 
-prob_1_18h = DFA(
-    states={'q0', 'q1', 'q2', 'q3', 'q4'},
-    input_symbols={'0', '1'},
-    transitions={
-        'q0': {'0': 'q4', '1': 'q1'},
-        'q1': {'0': 'q4', '1': 'q2'},
-        'q2': {'0': 'q4', '1': 'q3'},
-        'q3': {'0': 'q4', '1': 'q4'},
-        'q4': {'0': 'q4', '1': 'q4'},
-    },
-    initial_state='q0',
-    final_states={'q0', 'q1', 'q4'}
-)
-
+prob_1_18h = "(0|10)*"
 
