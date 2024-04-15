8812c092ba02006396c38b24c052100e5857b659
Jacob Lee <jkln3w@umsystem.edu>
2024-04-14 22:25:37 -0500

    Update file prob_1_19b.py

--- prob_1_19b.py
+++ prob_1_19b.py
@@ -1,17 +1,18 @@
 from automata.fa.nfa import NFA
 
 prob_1_19b = NFA(
-    states={'q0', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6'},
+    states={'q0', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7'},
     input_symbols={'0', '1'},
     transitions={
-        'q0': {'0': {'q1', 'q5'}, '': {'q0'}},  # Start "00" or "01" or loop on ε
-        'q1': {'0': {'q2'}},  # First "0" of "00"
-        'q2': {'1': {'q3'}},  # Move to handling "11" after "00"
-        'q3': {'1': {'q4'}},  # Complete "11"
-        'q4': {'': {'q0'}},  # Return to start, allowing repetition
-        'q5': {'1': {'q6'}},  # Handling "01"
-        'q6': {'': {'q0'}}   # Return to start, allowing repetition
+        'q0': {'0': {'q0', 'q1'}, '1': {'q2'}, '': {'q7'}},
+        'q1': {'0': {'q0'}, '1': {'q3'}},
+        'q2': {'1': {'q4'}},
+        'q3': {'0': {'q5'}},
+        'q4': {'0': {'q6'}},
+        'q5': {'1': {'q7'}},
+        'q6': {'0': {'q7'}, '1': {'q2'}},
+        'q7': {'0': {'q7'}, '': {'q0'}}
     },
     initial_state='q0',
-    final_states={'q0'}
+    final_states={'q0', 'q7'}
 )
