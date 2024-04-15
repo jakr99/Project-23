0fb970b44296d10d3eac14ada2c34fa73ee1d616
Jacob Lee <jkln3w@umsystem.edu>
2024-04-14 20:40:32 -0500

    Update file prob_1_19b.py

--- prob_1_19b.py
+++ prob_1_19b.py
@@ -4,13 +4,13 @@ prob_1_19b = NFA(
     states={'q0', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6'},
     input_symbols={'0', '1'},
     transitions={
-        'q0': {'0': {'q1', 'q5'}, '1': {'q0'}},  # Can start "00" or "01", or stay in q0 on "1"
+        'q0': {'0': {'q1', 'q5'}, '': {'q0'}},  # From q0, start "00" or "01", or stay on q0 by ε-transition (Kleene star effect)
         'q1': {'0': {'q2'}},  # Handling "00"
-        'q2': {'1': {'q3'}},  # Transitioning after "00" to handle "11"
-        'q3': {'1': {'q4'}, '0': {'q2'}},  # Completing "11", allows restarting "00"
-        'q4': {'': {'q0'}},  # Return to q0
+        'q2': {'1': {'q3'}},  # Transition to start "11" after "00"
+        'q3': {'1': {'q4'}},  # Complete "11"
+        'q4': {'': {'q0'}},  # Return to q0 to allow repetition of the pattern
         'q5': {'1': {'q6'}},  # Handling "01"
-        'q6': {'': {'q0'}}  # Return to q0
+        'q6': {'': {'q0'}},  # Return to q0 to allow repetition of the pattern
     },
     initial_state='q0',
     final_states={'q0'}
